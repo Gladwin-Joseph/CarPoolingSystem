74c1a9bfdc190aadd4d79df7df09da048245faf7
Chirag Singh <chiragrawat12@gmail.com>
2026-04-08 00:32:20 +0100

    Initial Task: Project Setup

---
+++ discovery-server/src/test/java/com/car_pooling_system/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.car_pooling_system.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ payment-service/src/main/java/com/car_pooling_system/paymentservice/PaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.car_pooling_system.paymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServiceApplication.class, args);
+	}
+
+}
